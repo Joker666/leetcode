17be0cb5bbbf654a67c1d95aabe916f9991c6d1e
Rafi <mr.k779@outlook.com>
2023-08-19 04:24:27 +0600

    Update 15_three_sum.py

--- 15_three_sum.py
+++ 15_three_sum.py
@@ -30,10 +30,32 @@ class Solution:
 
         return result
 
+    def three_sum_2(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        result: List[List[int]] = []
+
+        for i in range(len(nums)):
+            left = i + 1
+            right = len(nums) - 1
+
+            while left < right:
+                sum = nums[i] + nums[left] + nums[right]
+
+                if sum < 0:
+                    left += 1
+                elif sum > 0:
+                    right -= 1
+                else:
+                    res = [nums[i], nums[left], nums[right]]
+                    result.append(res)
+                    left += 1
+
+        return result
+
 
 solution = Solution()
-arr = [3, 0, -2, -1, 1, 2]
-s = solution.three_sum(arr)
+arr = [-1, 0, 1, 2, -1, -4]
+s = solution.three_sum_2(arr)
 print("\n\n")
 print(s)
 # Output: [[-1,-1,2],[-1,0,1]]
