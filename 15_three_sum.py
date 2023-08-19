4b5f0b6757a071e0e2511b329bea37b1f7cae5f0
Hasan <mr.k779@outlook.com>
2023-08-19 23:30:24 +0600

    Update 15_three_sum.py

--- 15_three_sum.py
+++ 15_three_sum.py
@@ -53,7 +53,7 @@ class Solution:
                     result.append(res)
                     left += 1
 
-                    if nums[left] == nums[left - 1] and left < right:
+                    while nums[left] == nums[left - 1] and left < right:
                         left += 1
 
         return result
@@ -66,20 +66,27 @@ print(s)
 # Output: [[-1,-1,2],[-1,0,1]]
 
 
-# First intuition was three loops which is not ideal
+# First intuition was three loops which is not ideal.
 # Second tried to store the pair of two number indices in a dictionary and
-# then computing the third index from that
+# then computing the third index from that.
 # Since it was open to the duplicate triplet issue, I sorted the triplet list and
-# Checked if it already exists in the final list or not
+# checked if it already exists in the final list or not.
 # It worked, but it failed in "Time limit exceeded" for such a poor solution
 
-# The second intuition is to use set and two sum to calculate, but I did not pursue that
-# I have solved that in 2021 with some help maybe
+# The second intuition is to use set and two sum to calculate, but I did not pursue that.
+# I have solved that in 2021 with some help maybe.
 
-# The third intuition I had was using two pointers, but I forgot how to go about that
+# The third intuition I had was using two pointers, but I forgot how to go about that.
 # Using one loop to go from left to right and then a nested one with two pointers
 # can solve the issue. One pointer would move from left and another from right. The question is
-# what condition will make the pointers move? Turns out to *zero* in on the target 0, if a sum is
+# what condition will make the pointers move?
+# The key here is sorting the array. That helps with two things. One to move the pointers, second
+# to remove duplicate triplets. 
+# Turns out to *zero* in on the target 0, if a sum is
 # greater than 0, we should move the right pointer to left and if it is lower than 0 then the left
 # pointer to the right.
 # This solves the pointer movement but still fails on the duplicate triplet issue.
+
+# To solve duplicate triplet issue, we will first try to skip already handled values.
+# When the array is sorted, we can skip the value if it's already handled.
+# Next, after storing the values that add upt to 0, we can skip the values that are the same.
