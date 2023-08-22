967132176250e96da129e168cb32a3b03c43bb0d
Hasan <mr.k779@outlook.com>
2023-08-23 02:22:26 +0600

    Update 459_repeated_substring_pattern.py

--- 459_repeated_substring_pattern.py
+++ 459_repeated_substring_pattern.py
@@ -1,8 +1,11 @@
 class Solution:
     def repeated_substring_pattern(self, s: str) -> bool:
-        substring = ""
-        for x in range(len(s) - 1):
-            substring = substring + s[x]
+        s_len = len(s)
+        for x in range(s_len - 1):
+            if s_len % (x + 1) != 0:
+                continue
+
+            substring = s[0:x+1]
             result = s.replace(substring, "")
 
             if result == "":
@@ -13,8 +16,9 @@ class Solution:
 
 solution = Solution()
 # s = "ababba" # False
-s = "babbabbabbabbab" # True
+# s = "babbabbabbabbab" # True
 # s = "ababab" # True
 # s = "abaababaab"  # True
+s = "bb"  # True
 
 print(solution.repeated_substring_pattern(s))
