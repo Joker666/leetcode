461ab8d83298c897db84a1cd26d1f388ce5ab054
Hasan <mr.k779@outlook.com>
2023-08-23 02:45:55 +0600

    Update 459_repeated_substring_pattern.py

--- 459_repeated_substring_pattern.py
+++ 459_repeated_substring_pattern.py
@@ -22,3 +22,17 @@ s = "babbabbabbabbab" # True
 # s = "bb"  # True
 
 print(solution.repeated_substring_pattern(s))
+
+# Even though this should be an easy problem, it took me several attempts. My first intuition was
+# storing character counts and reducing if the character is found, eventually if all character counts
+# were zero, we found substring pattern. This didn't work since characters can be sorted differently.
+
+# Second intuition was to go upto the middle and check left and right, if they match substring pattern.
+# However, this approach also didn't work since not always we will have substrings that repeat upto the center.
+
+# Third intuition came from cody that we iterate and find the smallest substring and try matching it.
+# I used string replace which was slower, but it got accepted.
+
+# It was a terrible solution and I had to look up editorial to find out that all substrings length will
+# be divisor of the full length,and you can skip the other ones. Also, there's no need to replace all
+# substrings, you can make the full string as you know the divisor number.
