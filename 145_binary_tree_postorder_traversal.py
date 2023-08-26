460a7b9b394ffb4e0921bf377827a2c2d01afd4d
Hasan <mr.k779@outlook.com>
2023-08-27 01:44:09 +0600

    Create 145_binary_tree_postorder_traversal.py

---
+++ 145_binary_tree_postorder_traversal.py
@@ -0,0 +1,48 @@
+from typing import List, Optional
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def inorder_traversal(self, root: Optional[TreeNode]) -> List[int]:
+        return self.traverse(root, [])
+
+    def traverse(self, node: Optional[TreeNode], f_list: List[int]) -> List[int]:
+        if not node:
+            return f_list
+
+        if node.left:
+            self.traverse(node.left, f_list)
+        if node.right:
+            self.traverse(node.right, f_list)
+
+        f_list.append(node.val)
+
+        return f_list
+
+
+solution = Solution()
+
+root = TreeNode(1)
+
+left = TreeNode(2)
+right = TreeNode(3)
+
+root.left = left
+root.right = right
+
+left.left = TreeNode(4)
+left.right = TreeNode(6)
+
+right_right = TreeNode(5)
+right.right = right_right
+
+
+s = solution.inorder_traversal(root)
+print(s)
