93fbbbf4b2ea5c489920500b62b743cf5cb1f23c
Hasan <mr.k779@outlook.com>
2023-09-06 22:52:56 +0600

    Update 24_swap_nodes_in_pairs.py

--- 24_swap_nodes_in_pairs.py
+++ 24_swap_nodes_in_pairs.py
@@ -13,11 +13,32 @@ class Solution:
             return head
 
         # Swap first two nodes
-        temp = head.next.next
-        head.next.next = head
-        head = head.next
+        temp = head.next.next  # save the third node or the next pair
+        head.next.next = head  # make the head tail to the second node
+        head = head.next  # put the second node in the head
         head.next.next = self.swap_pairs(temp)
         return head
+
+    def swap_pairs_2(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+
+        dummy = ListNode(0, head)
+        prev, curr = dummy, head
+
+        while curr:
+            # save ptrs
+            temp = curr.next.next  # save the third node or the next pair
+            second = curr.next  # save the second node
+
+            # reverse
+            second.next = curr  # second's next would be current
+            curr.next = temp  # curr is second now, so curr's next would be third
+            prev.next = second  # prev is dummy, prev's next is the head, which will be second
+
+            prev, curr = curr, temp
+
+        return dummy.next
 
     def list_print(self, linked_list: Optional[ListNode]):
         head = linked_list
@@ -38,7 +59,7 @@ l1.next.next.next = ListNode(4)
 # Before swap: 1 -> 2 -> 3 -> 4
 # After swap: 2 -> 1 -> 4 -> 3
 
-res1 = solution.swap_pairs(l1)
+res1 = solution.swap_pairs_2(l1)
 solution.list_print(res1)
 
 # Example 2:
