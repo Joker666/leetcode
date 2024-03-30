a022345a6e393667da0372a03d99077be03d1318
Rafi <mr.k779@outlook.com>
2024-03-31 05:57:12 +0600

    Create scratch.py

---
+++ scratch.py
@@ -0,0 +1,14 @@
+from typing import Dict
+
+
+def repeated_substring_pattern(s: str) -> bool:
+    for i in range(len(s) // 2):
+        substring = s[0:i + 1]
+        full_string = substring * (len(s) // (i+1))
+        if full_string == s:
+            return True
+
+    return False
+
+
+print(repeated_substring_pattern("ababba"))
