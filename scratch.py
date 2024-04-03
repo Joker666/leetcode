8a9bf1fef8873279aa700416c1dec26ef66704c4
Rafi <mr.k779@outlook.com>
2024-04-03 06:54:37 +0600

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -1,34 +1,44 @@
-from typing import List
+from typing import Optional
 
 
-def three_sum(nums: List[int]) -> List[List[int]]:
-    nums.sort()
-    print(nums)
-
-    triplets: List[List[int]] = []
-
-    nums_len = len(nums)
-
-    for i in range(nums_len):
-        if i > 0 and nums[i] == nums[i - 1]:
-            continue
-
-        left = i + 1
-        right = nums_len - 1
-
-        while left < right:
-            summation = nums[i] + nums[left] + nums[right]
-            if summation == 0:
-                triplets.append([nums[i], nums[left], nums[right]])
-                left += 1
-                while left < right and nums[left] == nums[left - 1]:
-                    left += 1
-            elif summation < 0:
-                left += 1
-            else:
-                right -= 1
-
-    return triplets
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 
-print(three_sum([-1, 0, 1, 2, -1, -4, -2, -3, 3, 0, 4]))
+def swap_pairs(head: Optional[ListNode]) -> Optional[ListNode]:
+    if head is None or head.next is None:
+        return head
+
+    dummy = curr = ListNode()
+    curr.next = head
+
+    while curr.next and curr.next.next:
+        prev = curr.next
+        curr.next = curr.next.next
+
+        prev.next = curr.next.next
+        curr.next.next = prev
+
+        curr = curr.next.next
+
+    return dummy.next
+
+
+def list_print(linked_list: Optional[ListNode]):
+    head = linked_list
+    while head:
+        print(head.val, end='')
+        if head.next:
+            print(" -> ", end='')
+        head = head.next
+    print("\n")
+
+
+l1 = ListNode(1)
+l1.next = ListNode(2)
+l1.next.next = ListNode(3)
+l1.next.next.next = ListNode(4)
+
+list_print(swap_pairs(l1))
